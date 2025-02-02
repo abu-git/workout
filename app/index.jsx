c293eafa9ccd057f64cd046386fe03aaf5f415e6
Samuel Abu <abu.sammie@gmail.com>
2025-02-02 12:04:46 +0200

    expo router installed and setup...App.js deleted and app dir created

---
+++ app/index.jsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, View, FlatList } from 'react-native'
+import exercises from '../assets/data/exercises.json'
+import ExerciseListItem from '../components/ExerciseListItem'
+
+
+export default function ExcersisesScreen() {
+
+    return(
+        <View style={styles.container}>
+            <StatusBar style="auto" />
+
+            <FlatList
+                data={exercises}
+                contentContainerStyle={{ gap: 5 }}
+                keyExtractor={(item, index) => item.name + index}
+                renderItem={({ item }) => <ExerciseListItem item={item} /> }
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'ghostwhite',
+      
+      justifyContent: 'center',
+      padding: 10,
+    }
+});
--- components/ExerciseListItem.jsx
+++ components/ExerciseListItem.jsx
@@ -5,23 +5,40 @@ export default function ExerciseListItem({ item }) {
   return(
     <View style={styles.exerciseContainer}>
       <Text style={styles.exerciseName}>{item.name}</Text>
-      <Text style={styles.exerciseSubtitle}>{item.muscle} | {item.equipment} </Text>
+      <Text style={styles.exerciseSubtitle}>
+        <Text style={styles.subValue}>{item.muscle}</Text> | <Text style={styles.subValue}>{item.equipment}</Text> 
+    </Text>
     </View> 
   )
 }
 
 const styles = StyleSheet.create({
     exerciseContainer: {
-      backgroundColor: '#fff',
-      padding: 10,
-      borderRadius: 10,
-      gap: 5
+        backgroundColor: '#fff',
+        padding: 10,
+        borderRadius: 10,
+        gap: 5,
+        marginHorizontal: 2,
+
+        //shadow
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
+
+        elevation: 2,
     },
     exerciseName: {
-      fontSize: 20, 
-      fontWeight: '500'
+        fontSize: 20, 
+        fontWeight: '500'
     },
     exerciseSubtitle: {
-      olor: 'dimgray'
+        
+    },
+    subValue: {
+        textTransform: 'capitalize'
     }
-  });
+});
